eeaf5afc600df1225ffc1fcc822e3c3975616d02
taewok <taewok5163@naver.com>
2023-10-11 01:14:13 +0900

    feat: font 소스 추가

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -1,9 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'IM_Hyemin-Bold';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2106@1.1/IM_Hyemin-Bold.woff2') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
     *{
         margin:0;
         padding:0;
+        z-index: 999;
     }
     html {
       scroll-behavior: smooth;
